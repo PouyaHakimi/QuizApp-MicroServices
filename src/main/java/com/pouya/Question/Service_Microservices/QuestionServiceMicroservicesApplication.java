d5cef311bbe25f6d159a380c8b2d0077bcde6196
PouyaHakimi <hakimifard.pouya@gmail.com>
2025-04-25 19:36:28 +0200

    first commit

---
+++ src/main/java/com/pouya/Question/Service_Microservices/QuestionServiceMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.pouya.Question.Service_Microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionServiceMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionServiceMicroservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pouya/Question/Service_Microservices/QuestionServiceMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pouya.Question.Service_Microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionServiceMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
